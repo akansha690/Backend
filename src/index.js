2b34bb6acfc5931ad43ac6941dc0de3abbed709c
akansha <akansha69@.com>
2024-12-07 13:54:19 +0530

    add utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv'
-import { dbconnect } from "../db/db.index.js";
 dotenv.config({
     path: './env'
 })
+
+import { dbconnect } from "../db/db.index.js";
+import { app } from './app.js';
+
 dbconnect()
+.then(()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log(`app is listening on Port :${process.env.PORT}`);
+    })
+}).catch((error)=>{
+    console.log("Mongodb connection failed", error);
+    
+})
