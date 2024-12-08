9e9440030294467ad6f6d20726c880c5d52ff1be
akansha <akansha69@.com>
2024-12-08 12:52:04 +0530

    add changes

--- models/user.model.js
+++ models/user.model.js
@@ -53,7 +53,7 @@ const userSchema = new mongoose.Schema(
 // Hook...
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next()
-    this.password= await bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next();    
 })
 // Custom Methods...
