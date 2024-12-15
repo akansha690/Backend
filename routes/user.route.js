71cfe74ac33db479fab7bf20a2cd9cf4d5d4098c
akansha <akansha69@.com>
2024-12-15 09:23:07 +0530

    add more models

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,6 @@ import { registerUser, loginUser, logoutUser, generateNewRefreshToken, changeCur
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
-
 const router = Router()
 
 router.route("/register").post(
